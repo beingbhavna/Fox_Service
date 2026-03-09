8b577341addaf4e64ad8ea067dda341057b80577
Bhavna Sharma <bhavna@i2k2.com>
2026-03-09 19:33:13 +0530

    updated code!!

--- fox_service_5Mar2026/src/app/pages/cart/cart.component.ts
+++ fox_service_5Mar2026/src/app/pages/cart/cart.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { CartService } from '../../services/cart.service';
 import { HeaderComponent } from '../../shared/header/header.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [CommonModule, HeaderComponent],
+  imports: [CommonModule,FormsModule, HeaderComponent],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.scss'
 })
@@ -15,6 +16,9 @@ export class CartComponent {
   gstPercent = 18; // example GST %
   cartItems: any[] = [];
   selectedCity: any;
+  showAddressPopup = false;
+  showAddressForm = false;
+  address: string = '';
 
   constructor(
     private cartService: CartService,
@@ -39,7 +43,9 @@ export class CartComponent {
   }
 
   continue() {
-    this.cartService.openModal();
+    this.showAddressPopup = true;
+    // open login modal at second time
+    // this.cartService.openModal();
   }
 
   backToHome() {
@@ -56,13 +62,58 @@ export class CartComponent {
 
   getTotalWithGST(): number {
     let subtotal = 0;
-
     this.cartItems.forEach(item => {
       subtotal += item.cities[0].pivot.price;
     });
-
     const gstAmount = (subtotal * this.gstPercent) / 100;
-
     return subtotal + gstAmount;
+  }
+
+  // openAddressPopup() {
+  //   this.showAddressPopup = true;
+  // }
+
+  closeAddressPopup() {
+    this.showAddressPopup = false;
+  }
+
+  openAddressForm() {
+    this.showAddressPopup = false;
+    this.showAddressForm = true;
+  }
+
+  backToPopup() {
+    this.showAddressForm = false;
+    this.showAddressPopup = true;
+  }
+
+  getAddressFromCoords(lat: number, lng: number) {
+  const apiKey = "YOUR_GOOGLE_MAP_API_KEY";
+  fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${apiKey}`)
+    .then(res => res.json())
+    .then(data => {
+      if (data.results.length > 0) {
+        this.address = data.results[0].formatted_address;
+      }
+    });
+}
+
+  getCurrentLocation() {
+    if (!navigator.geolocation) {
+      alert("Geolocation is not supported by your browser");
+      return;
+    }
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        console.log(latitude, longitude);
+        this.getAddressFromCoords(latitude, longitude);
+      },
+      (error) => {
+        console.error(error);
+        alert("Location permission denied");
+      }
+    );
   }
 }
